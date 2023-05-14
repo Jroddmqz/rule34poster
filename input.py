682e73c2fc02891080b5bbde80d3407df05e15c2
Jroddmqz <js.rdlf.mrqz.snts3@gmail.com>
2023-05-13 22:44:17 -0600

    Create input.py

---
+++ input.py
@@ -0,0 +1,12 @@
+tags = [
+    {
+        "tag": "honkai:_star_rail",
+        "channel": "-1001407356398",
+        "caption": "😈 https://t.me/+4wqeR0dRySJlMDUx 😈"
+    },
+    {
+        "tag": "genshin_impact",
+        "channel": "-1001629528057",
+        "caption": "😈 https://t.me/+45f-VAxVB2NjN2Jh 😈"
+    }
+]
